22e335bfaed97a0a9a2a03c42224840a14f4c055
塔城黒歌 <ener9621@gmail.com>
2026-08-27 15:55:52 +0900

    render対応

--- src/middlewares/ensure-authenticated.js
+++ src/middlewares/ensure-authenticated.js
@@ -5,7 +5,7 @@ function ensureAuthenticated() {
   return createMiddleware(async (c, next) => {
     const session = c.get('session');
     if(!session.user) {
-      return c.redirect('/login');
+      return c.redirect('/login?form=' + c.req.path);
     }
     await next();
   });
